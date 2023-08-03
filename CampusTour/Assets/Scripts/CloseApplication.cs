b6e016f18cf6809771cfcc2906d4850323230202
athomas2023 <97468398+athomas2023@users.noreply.github.com>
2023-08-02 22:36:41 -0400

    Input System Updated

--- CampusTour/Assets/Scripts/CloseApplication.cs
+++ CampusTour/Assets/Scripts/CloseApplication.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem; // Import the Input System namespace
 
 public class CloseApplication : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Reference to the Input Action asset. This will be set in the Unity Inspector.
+    [SerializeField]
+    private InputActionAsset escInputAction;
+
+    private void OnEnable()
     {
-        
+        // Enable the "Closeapp" action when this script is enabled
+        escInputAction.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        // Disable the "Closeapp" action when this script is disabled
+        escInputAction.Disable();
+    }
+
+    private void Update()
+    {
+        // Check if the "Esc" key is pressed
+        if (escInputAction != null && escInputAction.FindAction("Closeapp").triggered)
+        {
+            CloseApplicationMethod();
+        }
+    }
+
+    private void CloseApplicationMethod()
+    {
+        // Check if the game is running in the Unity editor or a standalone build
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false; // Exit Play mode in the editor
+        #else
+            Application.Quit(); // Quit the application in a standalone build
+        #endif
     }
 }
