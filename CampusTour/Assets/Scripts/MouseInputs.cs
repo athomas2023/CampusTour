abe24aac4ce1bed7e705b76e6c96492a0e6179f7
athomas2023 <97468398+athomas2023@users.noreply.github.com>
2023-11-10 16:27:11 -0500

    Update

--- CampusTour/Assets/Scripts/MouseInputs.cs
+++ CampusTour/Assets/Scripts/MouseInputs.cs
@@ -10,8 +10,8 @@ public class MouseInputs : MonoBehaviour
     [SerializeField]
     private InputActionAsset leftClickAction;
 
-    [SerializeField]
-    private TagManagerData tagManagerData;
+    //[SerializeField]
+    //private TagManagerData tagManagerData;
 
     private void OnEnable()
     {
@@ -30,41 +30,9 @@ public class MouseInputs : MonoBehaviour
 
     void Update()
     {
-        if (leftClickAction !=null && leftClickAction.FindAction("LeftClick").triggered)
+        if (leftClickAction != null && leftClickAction.FindAction("LeftClick").triggered)
         {
-            // Cast a ray from the camera's position in the direction of the mouse pointer
-            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                // Check if the hit object has one of the specified tags
-                if (ArrayContainsTag(tagManagerData.tags, hit.transform.tag))
-                {
-                    // Teleport the Controller GameObject to the hit point
-                    Controller.transform.position = hit.point;
-                }
-                else
-                {
-                    Debug.Log("Object hit but doesn't have the expected tag.");
-                }
-            }
-            else
-            {
-                Debug.Log("No object detected.");
-            }
+            // Place your teleportation or other logic here
         }
-    }
-
-    bool ArrayContainsTag(string[] tagArray, string tagToCheck)
-    {
-        foreach (string tag in tagArray)
-        {
-            if (tag == tagToCheck)
-            {
-                return true;
-            }
-        }
-        return false;
     }
 }
