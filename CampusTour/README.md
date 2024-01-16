f33eaa9d4d7a66e8d825c3c68bf3c3fae032b3f7
athomas2023 <97468398+athomas2023@users.noreply.github.com>
2024-01-15 20:52:00 -0500

    update

--- CampusTour/README.md
+++
@@ -1,9 +0,0 @@
-# CampusTour Unity Project
- BCU Campus Tour - Unity 2022.3.5f1  using the VR Template
-
- URP ( Universal Rendering Pipeline) Package
- First Person Starter Asset Package
- TextMeshPro Package
- Cinemachine Package
-
- Target Platform: WebGL HTML 5.
--- WebXR/WebXR-Campustour/Assets/VRBowling/Scripts/Bumper.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using Zinnia.Tracking.Collision;
-
-public class Bumper : MonoBehaviour {
-    public void Bounce(CollisionNotifier.EventData data) {
-        data.CollisionData.rigidbody.velocity = Vector3.Reflect(data.CollisionData.rigidbody.velocity, Vector3.right);
-    }
-}
